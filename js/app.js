136ac85393d85727e095279460a1b306c3ef92b0
Alejandro <cuentas@alejandrodiego.com>
2017-04-26 15:06:14 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -64337,64 +64337,61 @@ var Backbone = __webpack_require__(16);
 
 var models = {
 
-	panelget: function() {
+	panel: {
 
-		return new Backbone.Collection([
-			{
-				id: 'home',
-				link: '#home',
-				text: 'HOME',
-				icon: 'home'
-			},
-			{
-				id: 'page1',
-				link: '#page1',
-				text: 'PAGE 1',
-				icon: 'vcard'
-			},
-			{
-				id: 'page2',
-				link: '#page2',
-				text: 'PAGE 2',
-				icon: 'institution'
-			},
-			{
-				id: 'page3',
-				link: '#page3',
-				text: 'PAGE 3',
-				icon: 'picture-o'
-			},
-			{
-				id: 'page4',
-				link: '#page4',
-				text: 'PAGE 4',
-				icon: 'tv'
-			},
-			{
-				id: 'page5',
-				link: '#page5',
-				text: 'PAGE 5',
-				icon: 'user-circle'
-			},
-			{
-				id: 'page6',
-				link: '#page6',
-				text: 'PAGE 6',
-				icon: 'dashboard'
-			},
-			{
-				id: 'page7',
-				link: '#page7',
-				text: 'PAGE 7',
-				icon: 'globe'
-			},
-		]);
+		get: function() {
+
+			return new Backbone.Collection([
+				{
+					id: 'home',
+					link: '#home',
+					text: 'HOME'
+				},
+				{
+					id: 'page1',
+					link: '#page1',
+					text: 'PAGE 1'
+				},
+				{
+					id: 'page2',
+					link: '#page2',
+					text: 'PAGE 2'
+				},
+				{
+					id: 'page3',
+					link: '#page3',
+					text: 'PAGE 3'
+				},
+				{
+					id: 'page4',
+					link: '#page4',
+					text: 'PAGE 4'
+				},
+				{
+					id: 'page5',
+					link: '#page5',
+					text: 'PAGE 5'
+				},
+				{
+					id: 'page6',
+					link: '#page6',
+					text: 'PAGE 6'
+				},
+				{
+					id: 'page7',
+					link: '#page7',
+					text: 'PAGE 7'
+				},
+			]);
+
+		}
 
 	}
+
 };
 
 APP.reqres.setHandler('panel:get', function(params) {
-		return models.panelget();
+		return models.panel.get();
 });
 
 module.exports = models;
